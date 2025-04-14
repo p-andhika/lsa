07e746af0656450c0f96a1c432290b97e03ae5bc
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-14 14:35:06 +0700

    refactor: replace debounce logic with useDebounce hook

--- debounce/src/App.tsx
+++ debounce/src/App.tsx
@@ -1,17 +1,24 @@
-import { ChangeEvent } from "react";
+import { useState } from "react";
 import "./App.css";
 import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
-  const { debounce } = useDebounce();
+  // const { debounce } = useDebounce();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value, 500);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    debounce(() => console.log(e.target.value));
-  };
+  // const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  // debounce(() => console.log(e.target.value));
+  // };
 
   return (
     <>
-      <input type="text" placeholder="enter text" onChange={onChange} />
+      <input
+        type="text"
+        placeholder="enter text"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <p>{debouncedValue}</p>
     </>
   );
 }
--- debounce/src/hooks/useDebounce.ts
+++ debounce/src/hooks/useDebounce.ts
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useDebounce = () => {
-  const [timeoutId, setTimeoutId] = useState<undefined | number>();
+// export const useDebounce = () => {
+//   const [timeoutId, setTimeoutId] = useState<undefined | number>();
+//
+//   const debounce = (fn: Function, delay: number = 500) => {
+//     clearTimeout(timeoutId);
+//
+//     const id = setTimeout(() => {
+//       fn();
+//       setTimeoutId(undefined);
+//     }, delay);
+//
+//     setTimeoutId(id);
+//   };
+//
+//   return {
+//     debounce,
+//   };
+// };
 
-  const debounce = (fn: Function, delay: number = 500) => {
-    clearTimeout(timeoutId);
+export function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    const id = setTimeout(() => {
-      fn();
-      setTimeoutId(undefined);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
     }, delay);
 
-    setTimeoutId(id);
-  };
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
 
-  return {
-    debounce,
-  };
-};
+  return debouncedValue;
+}
