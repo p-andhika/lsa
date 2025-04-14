2ab81dee7ce8bfbf987dab090ae8de1ded8836f8
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-14 11:28:11 +0700

    feat: replace fetch with axios for data fetching

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,18 +1,16 @@
 import { useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-
-        const data = await response.json();
-        console.log(data);
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon/ditto",
+        );
+        console.log(response.data);
       } catch (error) {
         console.error("There was a problem", error);
       }
